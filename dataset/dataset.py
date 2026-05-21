3406b8149d070e254a2aa8893671b0006cf25932
daddo101 <daddo101@hotmail.com>
2026-05-21 04:44:01 +0000

    Added the malaria dataset class

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,1 +1,10 @@
+# import libraries
+import os
+
 print("this is our dataset class file")
+
+# we will create a dataset class to load our data and preprocess it
+# the malaria dataset is a collection of images of blood smears,
+# which are used to diagnose malaria. The dataset contains two classes:
+# infected and uninfected. Each class contains a set of images that are
+# labeled accordingly.
